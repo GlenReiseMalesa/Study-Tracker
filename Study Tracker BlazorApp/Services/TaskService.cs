9504c3230b545050f42dcb4031a508c6af9d2ece
glen malesa <malesaglen6@gmail.com>
2026-04-23 13:23:01 +0200

    add the read all and create database functionality

---
+++ Study Tracker BlazorApp/Services/TaskService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Study_Tracker_BlazorApp.Data;
+using Study_Tracker_BlazorApp.Models;
+
+namespace Study_Tracker_BlazorApp.Services
+{
+    //Contains our CRUD functions
+    public class TaskService
+    {
+        private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+
+
+        public TaskService(IDbContextFactory<AppDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        // READ : Get all tasks
+        public async Task<List<TaskItem>> GetAllTaskItemsAsync()
+        {
+            await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+            return await context.TaskItems.OrderByDescending(static T => T.CreatedAt).ToListAsync();
+        }
+        
+
+        //CREATE : add new taskitem
+        public async Task<TaskItem> CreateTaskItemAsync(TaskItem TaskItem)
+        {
+            await using var context = await _dbContextFactory.CreateDbContextAsync();
+            context.TaskItems.Add(TaskItem);
+            await context.SaveChangesAsync();
+            return TaskItem;
+        }
+    }
+}
