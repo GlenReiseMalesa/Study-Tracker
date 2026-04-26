867a6dee39bf6db39053519c25c2c7f541cb7d27
glen malesa <malesaglen6@gmail.com>
2026-04-26 02:32:04 +0200

    sqlitewasmhelper is keep the db persistant

--- Study Tracker BlazorApp/Program.cs
+++ Study Tracker BlazorApp/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using SqliteWasmHelper;
+using Study_Tracker_BlazorApp;
 using Study_Tracker_BlazorApp.Data;
 using Study_Tracker_BlazorApp.Services;
-using Study_Tracker_BlazorApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,10 +17,12 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 try
 {
     // Your service registration
-    builder.Services.AddDbContextFactory<AppDbContext>(options =>
-        options.UseSqlite("Data Source=app.db"));
-    builder.Services.AddScoped<DatabaseService>();
+    builder.Services.AddSqliteWasmDbContextFactory<AppDbContext>(
+        opts => opts.UseSqlite("Data Source=app.db"));
     builder.Services.AddScoped<TaskService>();
+    builder.Services.AddScoped<DatabaseService>();
+
+ 
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
     var host = builder.Build();
